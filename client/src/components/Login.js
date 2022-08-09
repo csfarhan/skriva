2aea5e6d566e42009a0580ae1a0a69350c96bb29
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-09 00:57:03 -0400

    Frontend mock login page implemented.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
-
-render(){
-    return (
-        <div>
-        </div>
-        )
+    constructor(props){
+      super(props);
+      this.state = {
+        email: '',
+        password: ''
+      }
     }
-}
-
+    emailChange = (event) =>{
+      this.setState({
+        email: event.target.value
+      })
+    }
+    passwordChange = (event) => {
+      this.setState({
+        password: event.target.value
+      })
+    }
+    handleSubmit = (event) =>{
+        alert(`Email: ${this.state.email}\nPassword: ${this.state.password}`)
+      }
+  
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label>Email:</label>
+              <input type='text' value={this.state.email} onChange={this.emailChange}/>
+            </div>
+            <div>
+              <label>Password:</label>
+              <input type='text' value={this.state.password} onChange={this.passwordChange}/>
+            </div>
+            <button type='submit'>Submit</button>
+          </form>
+        </div>
+      )
+    }
+  }
 export default Login

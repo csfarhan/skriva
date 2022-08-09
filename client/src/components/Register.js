82a393deed743a2a6ff09a475e54c86cf40e74e1
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-09 00:00:25 -0400

    Mock register page implemented.

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,11 +1,83 @@
-import React from 'react'
+import React, { Component } from 'react'
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker'
+import {useState, useEffect} from 'react';
 
-const Register = () => {
+const DateSet = () => {
+  const [startDate, setStartDate] = useState(new Date());
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
-  )
+    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+  );
+};
+
+class Register extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      date: ''
+    }
+  }
+  firstNameChange = (event) =>{
+    this.setState({
+      firstname: event.target.value
+    })
+  }
+  lastNameChange = (event) => {
+    this.setState({
+      lastname: event.target.value
+    })
+  }
+  emailChange = (event) =>{
+    this.setState({
+      email: event.target.value
+    })
+  }
+  passwordChange = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+  handleSubmit = (event) =>{
+    alert(`First Name: ${this.state.firstname}
+    \nLast Name: ${this.state.lastname}
+    \nEmail: ${this.state.email}
+    \nPassword: ${this.state.password}
+    \nDate: ${<DateSet/>}`)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>First Name:</label>
+            <input type='text' value={this.state.firstname} onChange={this.firstNameChange}/>
+          </div>
+          <div>
+            <label>Last Name:</label>
+            <input type='text' value={this.state.lastname} onChange={this.lastNameChange}/>
+          </div>
+          <div>
+            <label>Email:</label>
+            <input type='text' value={this.state.email} onChange={this.emailChange}/>
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type='text' value={this.state.password} onChange={this.passwordChange}/>
+          </div>
+          <div>
+            <label>Date of birth: </label>
+            <DateSet />
+          </div>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 

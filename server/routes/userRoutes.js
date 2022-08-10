d6b05584d8aea6b016373c6d7a0436acc7066d2e
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-09 20:04:42 -0400

    userRoutes put in same file

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 const {registerUser} = require("../controllers/userController")
+const auth = require("../middleware/auth");
 
 router.post("/",[check('firstName',
 'Firstname is required').not().isEmpty(),
@@ -9,5 +10,9 @@ check('lastName',
 'Lastname is required').not().isEmpty(),
 check('email', 'Please include a valid email').isEmail(),
 check('password','Please enter a password with 6 or more characters').isLength({ min: 6})], registerUser);
+
+router.post("/login", [auth,[check('email', 'Please include a valid email').isEmail(),
+check('password','Please enter a password with 6 or more characters').not().isEmpty()]], loginUser);
+
 
 module.exports = (router);

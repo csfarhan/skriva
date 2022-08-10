0454442303b89375d7ac32bf1208a5517955a7f5
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-10 02:54:22 -0400

    Get user tweet by tweet ID implemented.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -93,19 +93,16 @@ const followUser = asyncHandler(async (req, res)=>{
     const {toFollow} = req.body;
     // Init user who called a request
     const userId = req.user;
-    const userCurr = await User.findOne({_id: userId});
     const toFollowUser = await User.findOne({_id: toFollow});
 
     // Check if already followed
     if(toFollowUser.followers.indexOf(userId) > -1){
         return res.status(400).json({ msg: 'Already following'});
     }
-    // Add req.user to toFollow's followers array
-    toFollowUser.followers.push(userId);
-    // Add toFollow's userId to req.user's following array
-    userCurr.following.push(toFollow);
-    await User.updateOne({_id: userId}, {following: userCurr.following});
-    await User.updateOne({_id: toFollow}, {followers: toFollowUser.followers});
+
+    // Find user by id then push into followers/following array
+    await User.updateOne({_id: toFollowUser}, {$push:{followers: userId}});
+    await User.updateOne({_id: userId}, {$push:{following: toFollow}});
     return res.status(200).json({msg: 'Followed User'});
 
 });

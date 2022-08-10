298186c24a6b160ea8667dccde8d7463c6e5d254
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-10 02:53:59 -0400

    Get user tweet by tweet ID implemented.

--- server/routes/tweetRoutes.js
+++ server/routes/tweetRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 const {registerUser, loginUser} = require("../controllers/userController")
-const {postTweet, deleteTweet, updateTweet, likeTweet, unlikeTweet} = require("../controllers/tweetController")
+const {postTweet, deleteTweet, updateTweet, getTweet} = require("../controllers/tweetController")
 const auth = require("../middleware/auth");
 
 router.post("/postTweet", auth, postTweet);
@@ -11,9 +11,5 @@ router.delete("/deleteTweet", auth, deleteTweet);
 
 router.put("/updateTweet", auth, updateTweet);
 
-router.put("/addLike", likeTweet);
-
-router.put("/addDislike", unlikeTweet);
-
-
+router.get('/id', getTweet);
 module.exports = (router);

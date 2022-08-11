d619ee5c48be676742c4a8c67576f68432823c31
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-10 22:23:29 -0400

    Updating profile route/request has been implemented

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -9,11 +9,6 @@ const { check, validationResult} = require('express-validator');
 const updateProfile = asyncHandler(async (req, res) => {
     const {firstname, lastname, nickname, username, email, password, bio} = req.body;
     const userId = req.user;
-    const validateEmail = (email) => {
-        return email.match(
-          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-      };
     if(firstname){
         await User.updateOne({_id: userId}, {firstName: firstname});
     }
@@ -23,7 +18,7 @@ const updateProfile = asyncHandler(async (req, res) => {
     if(nickname){
         await User.updateOne({_id: userId}, {nickname: nickname});
     }
-    if(validateEmail(email)){
+    if(email){
         await User.updateOne({_id: userId}, {email: email});
     }
     if(bio){

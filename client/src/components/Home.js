eca7d8f4d5fb21ce502cfc80498360fc986947b8
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-08-20 00:21:46 -0400

    landingPage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,8 @@ import {useSelector, useDispatch} from 'react-redux';
 import {useNavigate} from 'react-router-dom';
 import {logout, reset} from '../features/auth/authSlice';
 import {Link} from 'react-router-dom'
+import './styling/home.css';
+import landing from './images/landing.png';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -16,6 +18,28 @@ const Home = () => {
   }
   return (
     <div>
+      <div className='container'>
+        <div className="innerContainer">
+          <div className='mainBox'>
+            <h1 className='signIn'>Sign in</h1>
+            <div className='textFields'>
+              <input className='textBox' type='text' placeholder='Email'></input>
+            </div>
+            <div className='textFields'>
+              <input className='textBox' type='text' placeholder='Password'></input>
+            </div>
+            <div className='center'>
+              <h6>Forgot Password?</h6>
+            </div>
+            <div className='button'>
+              <button type='submit'>Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    /*
+    <div>
       <h1>Homepage</h1>
       <p>
         {user ? (<button onClick={onLogout}>Logout</button>) : (
@@ -26,6 +50,7 @@ const Home = () => {
       <p>Don't have an account? <Link to='/register'>Sign Up</Link> </p>
       <p>Have an account? <Link to='/login'>Log in</Link> </p>
     </div>
+    */
   )
 }
 
